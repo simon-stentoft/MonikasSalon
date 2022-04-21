7a8d4828ec6da100824229506d213b42b09fbcac
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2022-04-21 10:32:25 +0200

    User

---
+++ src/sample/User.java
@@ -0,0 +1,100 @@
+package sample;
+
+public class User {
+    int userID;
+    String name;
+    String gender;
+    String email;
+    int phoneNumber;
+    String username;
+    String password;
+    Boolean worker;
+    String createdDate;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getWorker() {
+        return worker;
+    }
+
+    public void setWorker(Boolean worker) {
+        this.worker = worker;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", worker=" + worker +
+                ", createdDate='" + createdDate + '\'' +
+                '}';
+    }
+}
